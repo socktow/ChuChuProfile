f59b48c0c7103409a416ed462672b0494c3433e1
ChuChu Zihu Ms <mjssdn95@gmail.com>
2024-01-15 00:57:10 +0700

    dino home

---
+++ embed/builder.config.js
@@ -0,0 +1,47 @@
+/**
+ * This script file will (or atleast should) run before the main script file runs.
+ * This file should contain stuff like options, global variables (etc.) to be used by the main script.
+ */
+
+// Options
+
+// URL options can override the options below.
+// Options set through the menu can override both until the page is refreshed.
+options = {
+    username: 'Dino Star',
+    avatar: 'https://64.media.tumblr.com/971556f618a26e636afb943cf35d667f/756c0855440ca153-e6/s540x810/1bb0f8da30261722f234227b9e7d1250c552a678.gif',
+    verified: true,
+    noUser: false,
+    data: null,
+    guiTabs: ['author', 'description'],
+    useJsonEditor: false,
+    reverseColumns: false,
+    allowPlaceholders: false,
+    autoUpdateURL: false,
+    autoParams: false,
+    hideEditor: false,
+    hidePreview: false,
+    hideMenu: false,
+    single: false,
+    noMultiEmbedsOption: false,
+    sourceOption: false, // Display link to source code in menu.
+}
+
+// Default JSON object
+
+// json = {
+//     content: "Hello world",
+//     embed: {
+//         title: "A title",
+//         description: "A description",
+//     }
+// }
+
+
+// Write any code under the 'DOMContentLoaded' event to run after the page has loaded.
+addEventListener('DOMContentLoaded', () => {
+    // console.log('Hello 👋');
+
+    // Remove the colour picker
+    // document.querySelector('.colors').remove()
+})
